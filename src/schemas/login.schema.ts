bdbaa284e87c2d5b062c3a87b94b5d4c7b387854
albertBarsegyan <albertbarsegyan6@gmail.com>
2022-09-18 11:43:52 +0400

    feat: change pass validation

--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -2,5 +2,5 @@ import { object, string } from "yup";
 
 export const loginSchema = object({
   username: string().required().min(5),
-  password: string().required().min(8),
+  password: string().required().min(2),
 });
